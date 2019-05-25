1d54adad778513a01b5493213f685170e6dbdd12
astro7x <m.rahm7n@gmail.com>
2019-05-25 14:46:35 +0200

    [RESRV] the frame data type: TEST only with all possible inputs

--- src/qam_mapper.vhd
+++ src/qam_mapper.vhd
@@ -2,7 +2,8 @@
 -- QAM MAPPER
 ---------------------------------------------------------
 -- we want to have one single change of bits from one symbol to the other;
--- thus, using fixed point number[gray code] representation {00, 01, 11, 10} to represent {1+1j, -1+1j, -1-1j, 1-1j} 
+-- thus, using fixed point number[gray code] representation {00, 01, 11, 10} 
+-- to represent {1+1j, -1+1j, -1-1j, 1-1j} 
 -- Constellation	|	Symbols
 -- {00}			|	{+1+J}
 -- {01}			|	{-1+J}
@@ -13,26 +14,35 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+
 ENTITY qam_mapper is
 	port (
-		i_meta_sym		: in std_logic_vector (1 downto 0);		--i_meta_sym
-		I_symbols		: out std_logic_vector (1 downto 0);
-		Q_symbols		: out std_logic_vector (1 downto 0);
-		i_clk           : in  std_logic;
-    	i_rstb          : in  std_logic
-	);
-	
+		i_meta_sym		: in std_logic_vector (1 downto 0);		-- Input DATA
+		I_symbols		: out std_logic_vector (1 downto 0);	-- In-Phase Symbols
+		Q_symbols		: out std_logic_vector (1 downto 0);	-- Quadrature-Phase Symbols
+		i_clk           : in  std_logic;						-- system clock
+    	i_rstb          : in  std_logic 						-- Asynchronous active - high reset
+		);	
 END qam_mapper;
 
 ARCHITECTURE Behaviourl of qam_mapper is
 	-- define array for both I and Q parts (4 constellation points)
-	-- Constellation Size; to be generic however the modulation order
-	constant const_size : integer := 4;
-	constant frame_size : integer := 8;
+	-- Constellation Size; to be generic whatever the modulation order
+	CONSTANT const_size : integer := 4;
+	--+++++++RESERVED+++++++++++++++++++++++++++++++++++++++++++++
+	-- Could be tested with any frame Size (Binary Message Stream)
+	-- Here: We test against all possible input combination:
+	-- e.g 04-QAM: [00:11]
+	-- 	   16-QAM [0000: 1111] ...etc
+	-- constant frame_size : integer := 8;
+	-- Frame Length
+	-- TYPE frame is array (0 to frame_size-1) of std_logic;
+	--+++++++RESERVED+++++++++++++++++++++++++++++++++++++++++++++
+
 	-- Constellation Type
-	type constellation is array (0 to const_size-1) of signed (1 downto 0);
-	type frame is array (0 to frame_size-1) of std_logic;
-	
+	TYPE constellation is array (0 to const_size-1) of signed (1 downto 0);
+
+
 	-- I array
 	-- to_signed: Converts an INTEGER to a SIGNED vector of the specified SIZE.
 	constant I_data	: constellation := (
@@ -49,6 +59,7 @@ ARCHITECTURE Behaviourl of qam_mapper is
 		to_signed(-1,2)
 		); 
 	signal bits_in_unsigned : unsigned (1 downto 0);
+
 begin
 	-- to get from std_logic_vector to integer: > we go to unsigned then to integer 
 	-- s.t input data is needed to index the constellation array
